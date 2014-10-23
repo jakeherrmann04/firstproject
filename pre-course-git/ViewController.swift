8fe981f9bdb494e52b8a9f059e597305adbc4754
jakeherrmann04 <jake.herrmann04@gmail.com>
2014-10-22 18:44:11 -0600

    Initial Commit

---
+++ pre-course-git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pre-course-git
+//
+//  Created by Jake Herrmann on 10/22/14.
+//  Copyright (c) 2014 Jake Herrmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
