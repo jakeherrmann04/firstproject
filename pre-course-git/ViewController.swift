4742fc4a51456c79cf1151c9e0f2177e3ca6d114
jakeherrmann04 <jake.herrmann04@gmail.com>
2014-10-26 19:35:51 -0600

    logging hello world to console

--- pre-course-git/ViewController.swift
+++ pre-course-git/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.orangeColor()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
